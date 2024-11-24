deeaddccc2ea1fd5904154b63ed35628bf8e311c
aSumo <1xtelescope@gmail.com>
2024-11-24 16:05:08 +0900

    update: フォント切り替え機能を導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
+
   extends: ['@nuxt-themes/docus'],
   devtools: { enabled: true },
 
   modules: [
-    // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
+    '@nuxtjs/plausible',
+    '@nuxt/fonts'
   ],
-  compatibilityDate: '2024-10-24'
+
+  compatibilityDate: '2024-10-24',
+
+  css: ['~/assets/css/main.css']
 })
