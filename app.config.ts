248651e5e3fd11227290548b8344fc331b97aee4
aSumo <1xtelescope@gmail.com>
2024-11-24 12:53:50 +0900

    update: "Configuration"を完了

--- app.config.ts
+++ app.config.ts
@@ -1,39 +1,73 @@
-// https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
+
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
-    socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
-    },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
-    },
-    aside: {
-      level: 0,
-      collapsed: false,
-      exclude: []
-    },
-    main: {
-      padded: true,
-      fluid: true
-    },
+    title: 'aSumoranda',
+    description: 'ｱｽﾓのﾒﾓﾗﾝﾀﾞ、ｱｽﾓﾗﾝﾀﾞ',
+    url: 'https://asumoranda.vercel.app/',
+    layout: 'default',
+
     header: {
-      logo: true,
+      logo: false,
+      title: 'aSumoranda',
       showLinkIcon: true,
-      exclude: [],
-      fluid: true
+      fluid: true,
+    },
+
+    main: {
+      fluid: true,
+      padded: true,
+    },
+
+    aside: {
+      level: 1,
+      collapsed: false,
+    },
+
+    footer: {
+      credits: {
+        icon: 'heroicons:home',
+        text: '1x telescope',
+        href: 'https://sites.google.com/view/1xtelescope',
+      },
+      textLinks: [
+        {
+          text: 'CC BY-SA 4.0',
+          href: 'https://creativecommons.org/licenses/by-sa/4.0/',
+          icon: 'simple-icons:creativecommons',
+          rel: 'license',
+        }
+      ],
+      iconLinks: [
+        {
+          label: 'discord',
+          href: 'https://discord.app/users/userID',
+          icon: 'simple-icons:discord',
+          rel: 'noreferer noopener',
+        }, {
+          label: 'twitter',
+          href: 'https://x.com/asumo_1xts',
+          icon: 'simple-icons:twitter',
+          rel: 'noreferer noopener',
+        }, {
+          label: 'youtube',
+          href: 'https://www.youtube.com/@1xtelescope',
+          icon: 'simple-icons:youtube',
+          rel: 'noreferer noopener',
+        }, {
+          label: 'zenn',
+          href: 'https://zenn.dev/asumo_1xts',
+          icon: 'simple-icons:zenn',
+          rel: 'noreferer noopener',
+        }, {
+          label: 'github',
+          href: 'https://github.com/aSumo-1xts',
+          icon: 'simple-icons:github',
+          rel: 'noreferer noopener',
+        }
+      ],
+      fluid: true,
     }
+
   }
+  
 })
